224cabe8595ba1fa793f459d49cd4f9be4b165d2
oenegm <oenegm@gmail.com>
2024-01-21 20:30:53 +0200

    maps

--- slices/slices.go
+++ slices/slices.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	var s []string
@@ -20,5 +23,35 @@ func main() {
 	s = append(s, "d")
 	s = append(s, "e", "f")
 	fmt.Println("apd:", s)
-}
 
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("apd:", s)
+
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl1:", l)
+
+	l = s[2:]
+	fmt.Println("sl1:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	t2 := []string{"g", "h", "i"}
+	if slices.Equal(t, t2) {
+		fmt.Println("t == t2")
+	}
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d:", twoD)
+}
